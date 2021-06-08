a6d5bd64d04ba8431066542c3ed4e63ecb10ec6d
Ranjaka De Mel <ranjaka.demel@team.telstra.com>
2021-06-08 23:12:17 +1000

    feat: initial commit

---
+++ src/controllers/hello.controller.ts
@@ -0,0 +1,9 @@
+// import {inject} from '@loopback/core';
+import {get} from '@loopback/rest';
+
+export class HelloController {
+  @get('/hello')
+  hello(): string {
+    return 'Hello world!';
+  }
+}
